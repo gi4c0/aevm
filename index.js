5d552b6d878e57de9534ae5619ee81097095ddcd
alexpan <p.aleksey@do-it.co>
2018-11-12 10:21:47 +0200

    add js doc annotation

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const Joi = require('joi')
 
+/*
+ * Extract error message from joi output
+ * @param {object} req - The express request object
+ * @param {object} schema - Joi schema object
+ * @returns {string} Error message
+ */
 const getErrorMessage = (req, schema) => {
   const result = Joi.validate(req, schema)
   let message = ''
@@ -10,6 +16,11 @@ const getErrorMessage = (req, schema) => {
   return message
 }
 
+/*
+ * Take schema and returns express middleware which response with error
+ * @param {object} schema - Joi schema
+ * @returns {(req, res, next) =>}
+ */
 exports.validateWithResponse = schema => (req, res, next) => {
   const message = getErrorMessage(req, schema)
   if (message) {
@@ -19,6 +30,11 @@ exports.validateWithResponse = schema => (req, res, next) => {
   next()
 }
 
+/*
+ * Take schema and returns express middleware which pass error to next()
+ * @param {object} schema - Joi schema
+ * @returns {(req, res, next) =>}
+ */
 exports.validate = schema => (req, res, next) => {
   const message = getErrorMessage(req, schema)
   if (message) {
@@ -28,6 +44,9 @@ exports.validate = schema => (req, res, next) => {
   next()
 }
 
+/*
+ * Error handler middleware for default responses from joi errors
+ */
 exports.defaultErrorHandler = (err, req, res, next) => {
   if (process.env.NODE_ENV !== 'production') console.log(err)
   res.status(err.httpCode || 500).json({ message: err.message })
